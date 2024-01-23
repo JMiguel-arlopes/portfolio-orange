198ea2b2ad0c800d528ccf0b9a6975631db30ced
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-23 10:27:38 -0300

    inserção do componente FormLogin incorporando componentes InputsEmailPassword e Submit

---
+++ src/components/form/FormLogin/index.jsx
@@ -0,0 +1,21 @@
+import styles from "./formlogin.module.css";
+import { FcGoogle } from "react-icons/fc";
+import InputsEmailPassword from "../InputsEmailPassword";
+import Submit from "../Submit";
+
+export default function FormLogin() {
+  return (
+    <div className={styles.container_form_login}>
+      <a href="/" className={styles.sign_in_google}>
+        <FcGoogle />
+        Entrar com Google
+      </a>
+      <h3>Faça login com email</h3>
+      <InputsEmailPassword />
+      <Submit textSubmit="entrar" />
+      <div className={styles.sign_up}>
+        <a href="/register">Cadastre-se</a>
+      </div>
+    </div>
+  );
+}
