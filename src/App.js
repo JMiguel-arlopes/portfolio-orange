76d8a89991841802292821271d32e9c8ef95f803
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-24 11:53:15 -0300

    Apenas apagando linhas comentadas desnecessárias

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function App() {
   return (
     <div className="App">
-      {/* <h1>NavBar</h1> */}
       <Outlet />
-      {/* <h1>footer</h1> */}
     </div>
   );
 }
