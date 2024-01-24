b5598c0def1c4c808bfa01c17c6dddbbe037bda8
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-24 11:41:21 -0300

    Inserção de dados dos Input's no State
    - Função que insere os dados digitados nos inputs em um objeto ligado a um State
    - Função Submit que imprime o objeto mencionado no console.log, por enquanto

--- src/components/form/FormRegister/index.jsx
+++ src/components/form/FormRegister/index.jsx
@@ -2,13 +2,25 @@ import styles from "./formregister.module.css";
 import InputsEmailPassword from "../InputsEmailPassword";
 import Submit from "../Submit";
 import FormRowUsername from "../FormRowUsername";
+import { useState } from "react";
 
-export default function FormRegister() {
+export default function FormRegister({ handleSubmit }) {
+  const [newUser, setNewUser] = useState({});
+
+  const handleChangeText = (e) => {
+    setNewUser({ ...newUser, [e.target.name]: [e.target.value] });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    handleSubmit(newUser);
+  };
+
   return (
-    <div className={styles.container_form_register}>
-      <FormRowUsername />
-      <InputsEmailPassword />
+    <form className={styles.container_form_register} onSubmit={submit}>
+      <FormRowUsername handleOnChange={handleChangeText} user={newUser} />
+      <InputsEmailPassword handleOnChange={handleChangeText} user={newUser} />
       <Submit textSubmit="cadastrar" />
-    </div>
+    </form>
   );
 }
