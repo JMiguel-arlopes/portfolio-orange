346d28327fc8a5f49d8cca96a3e93ea9615ef100
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-24 11:58:44 -0300

    Inicialização estruturação componente Home
    - Adiciona controle de modal para adicionar projetos com useState.
    - Implementa lógica de exibição condicional do modal SetProjectModal.
    - Atualiza a estrutura do componente para melhor organização.
    - Introduz função changeTextTag para lidar com a alteração de tags.
    - Utiliza componente InputTag para entrada de texto de tags.
    - Utiliza componentes CardProfile, Header, FirstProjectCard, ContainerProjects e ProjectCard para representar a estrutura da página.
    - Conecta os componentes com dados fictícios de um arquivo db.json.
    - Adiciona imagens de perfil e projeto a partir de assets.
    
    Este commit inicia a interface do componente Home.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,61 @@
+import styles from "./home.module.css";
+import { useState } from "react";
+
+import CardProfile from "../../components/CardProfile";
 import Header from "../../components/Header";
+import InputTag from "../../components/InputTag";
+import FirstProjectCard from "../../components/FirstProjectCard";
+import SetProjectModal from "../../components/modal/SetProjectModal";
+import ContainerProjects from "../../components/ContainerProjects";
+import ProjectCard from "../../components/ProjectCard";
 
-
+import db from "../../db.json";
+import img_project from "../../assets/img_projeto.png";
+import img_profile from "../../assets/perfil.png";
 
 export default function Home() {
+  const [modalAddProject, setModalAddProject] = useState(false);
+
+  const changeTextTag = (e) => {
+    console.log(e.target.value);
+  };
+
+  const toggleAddProjectModal = () => {
+    setModalAddProject(!modalAddProject);
+  };
+
+  const currentUser = db.users[0];
+  const projectsDone = currentUser.projects;
+
   return (
     <div>
-        <Header/>
-           
-        
+      <Header />
+      <section className={styles.container_home}>
+        <div className={styles.content_profile}>
+          <CardProfile toggleModal={toggleAddProjectModal} />
+        </div>
+        <h3>Meus Projetos</h3>
+        <InputTag handleOnChange={changeTextTag} />
+        <ContainerProjects>
+          {projectsDone.length > 0 ? (
+            projectsDone.map((project) => {
+              return (
+                <ProjectCard
+                  name={currentUser.name}
+                  imgBackground={img_project}
+                  imgUser={img_profile}
+                  tags={project.tags}
+                />
+              );
+            })
+          ) : (
+            <FirstProjectCard toggleModal={toggleAddProjectModal} />
+          )}
+        </ContainerProjects>
+      </section>
+      {modalAddProject && (
+        <SetProjectModal toggleModal={toggleAddProjectModal} />
+      )}
     </div>
   );
 }
