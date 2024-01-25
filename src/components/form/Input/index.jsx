cdfb4e54b88b3efd75f70513ccfcf23522995f5f
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-25 13:29:15 -0300

    Aprimorando UX do Input
    - inserindo State para averiguar quando Input foi Focado
    - retira prop do Placeholder já que a Label já irá fazer essa funcionalidade
    - Insere animação com CSS para melhor UX

--- src/components/form/Input/index.jsx
+++ src/components/form/Input/index.jsx
@@ -1,24 +1,26 @@
+import { useState } from "react";
 import styles from "./input.module.css";
 
-export default function Input({
-  text,
-  name,
-  type,
-  placeholder,
-  handleOnChange,
-  value,
-}) {
+export default function Input({ text, name, type, handleOnChange, value }) {
+  const [isFocus, setFocus] = useState(false);
+
+  const handleInputFocus = () => {
+    setFocus(true);
+  };
+
   return (
     <div className={styles.input_container}>
-      <label htmlFor={name}>{text}</label>
+      <label htmlFor={name} className={isFocus ? styles.up : ""}>
+        {text}
+      </label>
       <input
         required="true"
         type={type}
         id={name}
         name={name}
-        placeholder={placeholder}
         onChange={handleOnChange}
         value={value}
+        onFocus={handleInputFocus}
       />
     </div>
   );
