de2f6a7e80cbcad6f5f1c045bc100fd8a2a9c41d
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-25 14:36:35 -0300

    mudando roda do link 'descobrir'

---
+++ src/components/layoult/Header/index.jsx
@@ -0,0 +1,34 @@
+import styles from "./header.module.css";
+import Logo from "../../../assets/Logo orange.svg";
+import Perfil from "../../../assets/perfil.png";
+import Icon from "../../../assets/icon.svg";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <div className={styles.container_header}>
+      <div className={styles.menuLogo}>
+        <img src={Logo} />
+
+        <div className={styles.menu}>
+          <Link className={styles.links} to={"/home"}>
+            Meus projetos
+          </Link>
+          <Link className={styles.links} to={"/explore"}>
+            {" "}
+            Descobrir
+          </Link>
+        </div>
+      </div>
+
+      <div className={styles.perfil}>
+        <div className={styles.subnav}>
+          <img className={styles.perfilImg} src={Perfil} />
+          <Link className={styles.sair} to={"/login"}>
+            Sair
+          </Link>
+        </div>
+        <img src={Icon} />
+      </div>
+    </div>
+  );
+}
