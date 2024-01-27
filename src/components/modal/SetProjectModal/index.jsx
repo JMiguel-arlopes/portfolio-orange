b7eddb9d08381b07f99b0ebac57bbdb51244d479
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-27 18:20:59 -0300

    Insere Modal de confirmação para o usuário
    - Insere novo componente: ModalSucess
    - muda a função handleSubmit para inserir o novo modal
    - insere uma condicional ternária para alternar entre formulário e modal de sucesso

--- src/components/modal/SetProjectModal/index.jsx
+++ src/components/modal/SetProjectModal/index.jsx
@@ -4,10 +4,12 @@ import FormNewProject from "../../form/FormNewProject";
 import { useState } from "react";
 import InputImage from "../../form/InputImage";
 import ContainerModal from "../ContainerModal";
+import ModalSucess from "../ModalSucess";
 
 export default function SetProjectModal({ toggleModal }) {
   const outModal = "outmodal";
   const [formData, setFormData] = useState({});
+  const [sucess, setSucess] = useState(false);
 
   const disabledModal = (e) => {
     if (e.target.id === outModal) {
@@ -23,35 +25,47 @@ export default function SetProjectModal({ toggleModal }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Dados do formulário:", formData);
-    toggleModal();
+    setSucess(true);
   };
 
   return (
-    <ContainerModal id={outModal} handleOnCLick={disabledModal}>
-      <form className={styles.modal} onSubmit={handleSubmit}>
-        <h1>Adicionar projeto</h1>
+    <>
+      {!sucess ? (
+        <ContainerModal id={outModal} handleOnCLick={disabledModal}>
+          <form className={styles.modal} onSubmit={handleSubmit}>
+            <h1>Adicionar projeto</h1>
 
-        <div className={styles.container}>
-          <InputImage />
+            <div className={styles.container}>
+              <InputImage />
 
-          <div className={styles.form}>
-            <FormNewProject handleChange={handleChange} formData={formData} />
-          </div>
-        </div>
+              <div className={styles.form}>
+                <FormNewProject
+                  handleChange={handleChange}
+                  formData={formData}
+                />
+              </div>
+            </div>
 
-        <a className={styles.visualizar}>Visualizar publicação</a>
+            <a className={styles.visualizar}>Visualizar publicação</a>
 
-        <div className={styles.buttons}>
-          <input type="submit" className={styles.salvar} value="salvar" />
-          <button
-            id={outModal}
-            onClick={disabledModal}
-            className={styles.cancelar}
-          >
-            Cancelar
-          </button>
-        </div>
-      </form>
-    </ContainerModal>
+            <div className={styles.buttons}>
+              <input type="submit" className={styles.salvar} value="salvar" />
+              <button
+                id={outModal}
+                onClick={disabledModal}
+                className={styles.cancelar}
+              >
+                Cancelar
+              </button>
+            </div>
+          </form>
+        </ContainerModal>
+      ) : (
+        <ModalSucess
+          message="Projeto adicionado com sucesso!"
+          handleOnClick={toggleModal}
+        />
+      )}
+    </>
   );
 }
