7f4898fec156ea0a81403300f52ad225b7992fee
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-26 23:51:25 -0300

    Insere formulário e captura dados do usuário
    - Insere handleChange para capturar dados do Input
    - insere handleSubmit para imprimir os dados no console.log ao evento Submit ocorrer
    - Muda de Button para um Input do tipo Submit
    - Aprimora o CSS

--- src/components/modal/SetProjectModal/index.jsx
+++ src/components/modal/SetProjectModal/index.jsx
@@ -1,14 +1,27 @@
 import styles from "./setProjectModal.module.css";
-import Collections from "../../../assets/collections.svg"
-import FormNewProject from "../../form/FormNewProject"
+import Collections from "../../../assets/collections.svg";
+import FormNewProject from "../../form/FormNewProject";
+import { useState } from "react";
 
 export default function SetProjectModal({ toggleModal }) {
   const outModal = "outmodal";
+  const [formData, setFormData] = useState({});
 
   const disabledModal = (e) => {
     if (e.target.id === outModal) {
       toggleModal();
     }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: [value] });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Dados do formulário:", formData);
+    toggleModal();
   };
 
   return (
@@ -17,35 +30,39 @@ export default function SetProjectModal({ toggleModal }) {
       id={outModal}
       onClick={disabledModal}
     >
-      <div className={styles.modal}>
+      <form className={styles.modal} onSubmit={handleSubmit}>
         <h1>Adicionar projeto</h1>
 
         <div className={styles.container}>
           <div className={styles.image}>
             <p>Selecione o conteúdo que você deseja fazer upload</p>
-            <div class={styles.uploudImage} >
-                <img src={Collections} />
-                <label for="upload">Compartilhe seu talento com milhares de pessoas</label>
-                <input type="file" id="upload" name="upload" />
+            <div class={styles.uploudImage}>
+              <img src={Collections} />
+              <label for="upload">
+                Compartilhe seu talento com milhares de pessoas
+              </label>
+              <input type="file" id="upload" name="upload" />
             </div>
           </div>
 
           <div className={styles.form}>
-              <FormNewProject/>
-          </div>  
+            <FormNewProject handleChange={handleChange} formData={formData} />
+          </div>
         </div>
 
         <a className={styles.visualizar}>Visualizar publicação</a>
 
         <div className={styles.buttons}>
-          <button className={styles.salvar}>Salvar</button>
-          <button 
-          id={outModal}
-          onClick={disabledModal}
-          className={styles.cancelar}>Cancelar</button>
-          
+          <input type="submit" className={styles.salvar} value="salvar" />
+          <button
+            id={outModal}
+            onClick={disabledModal}
+            className={styles.cancelar}
+          >
+            Cancelar
+          </button>
         </div>
-      </div>
+      </form>
     </section>
   );
 }
