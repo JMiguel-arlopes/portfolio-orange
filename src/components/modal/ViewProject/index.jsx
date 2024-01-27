223c98f5da9dcee7cef64318d5c48a18e099a948
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-27 13:44:22 -0300

    Cria o componente ViewProject
    - Implementa o componente ViewProject, que represent a visualização de um projeto
    - Integra o componente MiniatureProfile
    
    Este commit cria o componente ViewProject e é apenas um esboço para estilização antes de receber os dados

---
+++ src/components/modal/ViewProject/index.jsx
@@ -0,0 +1,46 @@
+import styles from "./viewProject.module.css";
+import MiniatureProfile from "../../layoult/MiniatureProfile";
+import Tag from "../../layoult/Tag";
+import ContainerModal from "../ContainerModal";
+
+import img_bg from "../../../assets/img_projeto.png";
+
+export default function ViewProject({ handleOnClick }) {
+  const outModal = "outmodal";
+
+  const disabledModal = (e) => {
+    if (e.target.id === outModal) {
+      console.log(e.target);
+      handleOnClick();
+    }
+  };
+
+  return (
+    <ContainerModal id={outModal} handleOnCLick={disabledModal}>
+      <div className={styles.modal_view_project}>
+        <div className={styles.row_information}>
+          <MiniatureProfile />
+          <h2>Ecommerce One Page</h2>
+          <div className={styles.tags}>
+            <Tag text="Tag1" />
+            <Tag text="Tag2" />
+            <Tag text="Tag3" />
+          </div>
+        </div>
+        <div className={styles.image_project}>
+          <img src={img_bg} alt="image project" />
+        </div>
+        <div className={styles.general_description}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
+            ullam temporibus autem odit inventore nihil, molestias illum eveniet
+            illo nisi corporis? Distinctio numquam maiores minima placeat.
+            Placeat voluptates laborum eveniet!
+          </p>
+          <h5>Download</h5>
+          <a href="/">www.exemplo.com/kiplin</a>
+        </div>
+      </div>
+    </ContainerModal>
+  );
+}
