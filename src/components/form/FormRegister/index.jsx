ed997e3bb3f4ba8b222d7b3b97de8eae98bf3574
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-28 10:19:17 -0300

    Refatora código colocando props no Componente Pai
    - implementa handleOnChange, dataUser, textSubmit nas props para melhorar sintaxe
    - retira handleChangeText e State de newUser enquanto recebe esses dados e funcionalidades do Componente Pai
    - mantém apenas o handleSubmit

--- src/components/form/FormRegister/index.jsx
+++ src/components/form/FormRegister/index.jsx
@@ -2,25 +2,23 @@ import styles from "./formregister.module.css";
 import InputsEmailPassword from "../InputsEmailPassword";
 import Submit from "../Submit";
 import FormRowUsername from "../FormRowUsername";
-import { useState } from "react";
 
-export default function FormRegister({ handleSubmit }) {
-  const [newUser, setNewUser] = useState({});
-
-  const handleChangeText = (e) => {
-    setNewUser({ ...newUser, [e.target.name]: [e.target.value] });
-  };
-
+export default function FormRegister({
+  handleSubmit,
+  handleOnChange,
+  dataUser,
+  textSubmit,
+}) {
   const submit = (e) => {
     e.preventDefault();
-    handleSubmit(newUser);
+    handleSubmit();
   };
 
   return (
     <form className={styles.container_form_register} onSubmit={submit}>
-      <FormRowUsername handleOnChange={handleChangeText} user={newUser} />
-      <InputsEmailPassword handleOnChange={handleChangeText} user={newUser} />
-      <Submit textSubmit="cadastrar" />
+      <FormRowUsername handleOnChange={handleOnChange} user={dataUser} />
+      <InputsEmailPassword handleOnChange={handleOnChange} user={dataUser} />
+      <Submit textSubmit={textSubmit} />
     </form>
   );
 }
