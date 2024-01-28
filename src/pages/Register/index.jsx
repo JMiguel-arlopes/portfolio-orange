928d296a47f3c30d325007d892cefd479194af68
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-28 10:28:27 -0300

    Implementa funcionalidades com requisição ao Register
    - utiliza a página de cadastro para os usuários criar novas contas.
    - implementa lógica de cadastro ao enviar uma requisição POST para o servidor utilizando a biblioteca axios.
    - utiliza useState para armazenar informações do novo usuário, texto do botão de Submit e State de notificação de sucesso.
    - exibe uma notificação de sucesso ao usuário após um cadastro bem-sucedido, utilizando o componente Notification
    - utiliza a biblioteca react-router-dom para navegar para a página de login após o cadastro bem-sucedido

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,19 +1,53 @@
-import ContainerImage from "../../components/layoult/ContainerImage";
 import styles from "./register.module.css";
+import ContainerImage from "../../components/layoult/ContainerImage";
 import img_register from "../../assets/img_cadastro.png";
 import FormRegister from "../../components/form/FormRegister";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Notification from "../../components/layoult/Notification";
 
 export default function Register() {
-  const signUp = (newUser) => {
-    console.log(newUser);
+  let navigate = useNavigate();
+  const [newUser, setNewUser] = useState({});
+  const [textSubmit, setTextSubmit] = useState("Cadastrar");
+  const [notificationSucess, SetNotificationSucess] = useState(false);
+
+  const signUp = async () => {
+    setTextSubmit("Aguarde...");
+
+    await axios
+      .post("http://localhost:8080/users", newUser)
+      .then(() => {
+        SetNotificationSucess(true);
+        setTimeout(() => {
+          setTextSubmit("Cadastrar");
+          navigate("/login");
+        }, 100);
+      })
+      .catch((error) => {
+        console.log("erro ao cadastrar: ", error);
+      });
+  };
+
+  const handleChangeText = (e) => {
+    setNewUser({ ...newUser, [e.target.name]: e.target.value });
   };
 
   return (
     <section className={styles.container_register}>
       <ContainerImage img={img_register} alt="imagem do cadastro" />
       <div className={styles.content_register}>
+        {notificationSucess && (
+          <Notification status="sucess" message="Cadastro feito com sucesso" />
+        )}
         <h2>Cadastre-se</h2>
-        <FormRegister handleSubmit={signUp} />
+        <FormRegister
+          handleSubmit={signUp}
+          handleOnChange={handleChangeText}
+          dataUser={newUser}
+          textSubmit={textSubmit}
+        />
       </div>
     </section>
   );
