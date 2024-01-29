e4e60647b7855f88df620b632e44fa175e179b11
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-29 14:34:44 -0300

    Inserindo setTimeOut para desativar notificação
    - Insere propriedade de ativação/desativação do componente
    - Implemente um setTimeOut para inverter o valor booleano trazido pela prop

--- src/components/layoult/Notification/index.jsx
+++ src/components/layoult/Notification/index.jsx
@@ -1,7 +1,11 @@
 import styles from "./notification.module.css";
 import check from "../../../assets/CheckCircleOutlined.svg";
 
-export default function Notification({ status, message }) {
+export default function Notification({ status, message, toggleNotification }) {
+  setTimeout(() => {
+    toggleNotification();
+  }, 3200);
+
   return (
     <div className={`${styles.notification} ${styles[status]}`}>
       <img src={check} alt="check" />
