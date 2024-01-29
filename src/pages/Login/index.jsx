f0a0e32f4be25f7b326ead8604dfe5516e5b06ea
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-29 14:37:21 -0300

    Insere requisição HTTP, verificação de usuário e notificação
    - Permite, agora, aos usuários autenticarem-se no Orange Portfólio.
    - Implementa lógica de autenticação ao enviar uma requisição GET para o servidor utilizando a biblioteca axios.
    - altera State do componente com useState para armazenar informações de entrada do usuário e estado de notificação de erro
    - Utiliza o contexto UserContext para armazenar informações do usuário logado após a autenticação bem-sucedida
    - Exibe uma notificação de erro ao usuário quando as credenciais não são encontradas
    - Utiliza a biblioteca react-router-dom para navegar para a página de home após a autenticação bem-sucedida
    
    Este commit implementa a funcionalidade completa da página de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,18 +2,66 @@ import styles from "./login.module.css";
 import img_login from "../../assets/img_login.svg";
 import FormLogin from "../../components/form/FormLogin";
 import ContainerImage from "../../components/layoult/ContainerImage";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { UserContext } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
+import Notification from "../../components/layoult/Notification";
 
 export default function Login() {
-  const singIn = (user) => {
-    console.log("Registro feito com sucesso: ", user);
+  let navigate = useNavigate();
+  const [dataInput, setDataInput] = useState({});
+  const [notificationError, setNotificationError] = useState(false);
+  const { loggedUser, setLoggedUser } = useContext(UserContext);
+
+  const singIn = async () => {
+    await axios
+      .get("http://localhost:8080/users")
+      .then((resp) => {
+        const users = resp.data;
+
+        users.forEach((user) => {
+          const email = user.email;
+          const password = user.password;
+
+          if (email === dataInput.email && password === dataInput.password) {
+            setLoggedUser(user);
+            return navigate("/home");
+          }
+        });
+      })
+      .then(() => {
+        toggleNotification();
+        setDataInput({});
+      })
+      .catch((error) => console.log("Usuário não encontrado: ", error));
+  };
+
+  const handleChange = (e) => {
+    setDataInput({ ...dataInput, [e.target.name]: e.target.value });
+  };
+
+  const toggleNotification = () => {
+    setNotificationError(!notificationError);
   };
 
   return (
     <section className={styles.container_login}>
       <ContainerImage img={img_login} alt="Imagem do login" />
       <div className={styles.content_login}>
+        {notificationError && (
+          <Notification
+            message="Email ou Senha não encontrados!"
+            status="error"
+            toggleNotification={toggleNotification}
+          />
+        )}
         <h2>Entre no Orange Portfólio</h2>
-        <FormLogin handleSubmit={singIn} />
+        <FormLogin
+          handleSubmit={singIn}
+          handleOnChange={handleChange}
+          dataInput={dataInput}
+        />
       </div>
     </section>
   );
