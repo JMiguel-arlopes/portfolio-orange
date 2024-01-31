2d454f6e40fc4db51fd46b71169e46974f7c423f
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-30 22:17:23 -0300

    Atualiza a página Home - "meus projetos"
    - Refatora a lógica de gerenciamento de projetos, introduzindo um novo State `projectsDone` para manter uma cópia dos projetos originais
    - Adiciona funcionalidades de edição e exclusão de projetos utilizando os métodos `editProject` e `deleteProject`, com auxilio do `axios` para requisições Patch
    - Utiliza o hook `uuidv4` para gerar IDs únicos para os novos projetos
    - Adapta o componente `ProjectCard` para `ProjectCardHome` para melhor refletir a nova estrutura e comportamento
    - Melhora a filtragem de projetos por tags e atualiza a lista de projetos visíveis em tempo real
    - Aprimora a criação de projetos para aceitar tags separadas por vírgula ou espaços
    - Atualiza o modal `SetProjectModal` para utilizar a função `uuidv4` na criação de novos projetos

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,19 @@
 import styles from "./home.module.css";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 import CardProfile from "../../components/cards/CardProfile";
 import Header from "../../components/layoult/Header";
 import FirstProjectCard from "../../components/cards/FirstProjectCard";
 import SetProjectModal from "../../components/modal/SetProjectModal";
 import ContainerProjects from "../../components/layoult/ContainerProjects";
-import ProjectCard from "../../components/cards/ProjectCard";
-import axios from "axios";
+import ProjectCardHome from "../../components/cards/ProjectCardHome";
+import Input from "../../components/form/Input";
 
-// import db from "../../db.json";
 import img_project from "../../assets/img_projeto.png";
 import img_profile from "../../assets/perfil.png";
-import Input from "../../components/form/Input";
 
 export default function Home() {
   const { id } = useParams();
@@ -21,6 +21,7 @@ export default function Home() {
 
   const [currentUser, setCurrentUser] = useState({});
   const [visibleProjects, setVisibleProjects] = useState([]);
+  const [projectsDone, setProjectsDone] = useState([]);
   const [modalAddProject, setModalAddProject] = useState(false);
 
   useEffect(() => {
@@ -30,6 +31,7 @@ export default function Home() {
         .then((response) => {
           const user = response.data;
           setCurrentUser(user);
+          setProjectsDone(user.projects);
           setVisibleProjects(user.projects);
         })
         .catch((err) => console.error(err));
@@ -41,7 +43,7 @@ export default function Home() {
   const filterProjectsByTag = (e) => {
     const word = e.target.value.toLowerCase();
 
-    const updateProjects = visibleProjects.filter((project) => {
+    const updateProjects = projectsDone.filter((project) => {
       const tagsProject = project.tags.map((tag) => tag.toLowerCase());
       return tagsProject.some((tag) => tag.includes(word));
     });
@@ -53,12 +55,52 @@ export default function Home() {
     setModalAddProject(!modalAddProject);
   };
 
-  const addProject = async (newProject) => {
+  const addProject = async (project) => {
     const result = currentUser;
+    const newProject = { ...project };
+    newProject.tags = newProject.tags.split(/[,\s]+/);
+    newProject.id = uuidv4();
+
     result.projects = [...result.projects, newProject];
 
     await axios
       .patch(url, result)
+      .then((resp) => {
+        setCurrentUser(resp.data);
+        setVisibleProjects(resp.data.projects);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const editProject = async (project) => {
+    const copyUser = { ...currentUser };
+    const newProject = { ...project };
+    newProject.tags = newProject.tags.split(/[,\s]+/);
+
+    copyUser.projects = copyUser.projects.filter(
+      (projectFiltered) => projectFiltered.id !== newProject.id
+    );
+
+    copyUser.projects.unshift(newProject);
+
+    await axios
+      .patch(url, copyUser)
+      .then((resp) => {
+        setCurrentUser(resp.data);
+        setVisibleProjects(resp.data.projects);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const deleteProject = async (project) => {
+    const copyUser = { ...currentUser };
+
+    copyUser.projects = copyUser.projects.filter((projectFiltered) => {
+      return projectFiltered.id !== project.id;
+    });
+
+    await axios
+      .patch(url, copyUser)
       .then((resp) => {
         setCurrentUser(resp.data);
         setVisibleProjects(resp.data.projects);
@@ -85,10 +127,12 @@ export default function Home() {
           required={false}
         />
         <ContainerProjects>
-          {visibleProjects.length > 0 ? (
+          {projectsDone.length > 0 ? (
             visibleProjects.map((project) => {
               return (
-                <ProjectCard
+                <ProjectCardHome
+                  dataProject={project}
+                  id={project.id}
                   name={currentUser.firstName + " " + currentUser.lastName}
                   imgBackground={img_project}
                   imgUser={img_profile}
@@ -96,6 +140,8 @@ export default function Home() {
                   tags={project.tags}
                   link={project.link}
                   description={project.description}
+                  handleEdit={editProject}
+                  handleDelete={deleteProject}
                 />
               );
             })
