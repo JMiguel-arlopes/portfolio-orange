7d3c8169c9ead851d7fe3c06c7d2f23b6b59a325
JM-arlopes <jmiguelarlopes@gmail.com>
2024-02-01 21:26:43 -0300

    Aprimora o componente InputImage
    - Adiciona suporte para exibição de uma imagem prévia, caso haja uma imagem carregada
    - Adiciona a prop `handleUpload` para lidar com a mudança de imagem
    
    Este commit melhora o componente InputImage, adicionando suporte para exibição de imagem prévia

--- src/components/form/InputImage/index.jsx
+++ src/components/form/InputImage/index.jsx
@@ -1,15 +1,33 @@
 import styles from "./inputImage.module.css";
 import Collections from "../../../assets/collections.svg";
 
-export default function InputImage() {
+export default function InputImage({ handleUpload, dataImage }) {
+  const toggleDisplay = dataImage ? { display: "none" } : { display: "block" };
+
+  const style = {
+    backgroundImage: dataImage ? `url(${URL.createObjectURL(dataImage)})` : "",
+  };
+
   return (
     <div className={styles.image}>
       <p>Selecione o conteúdo que você deseja fazer upload</p>
-      <label htmlFor="upload">
-        <div className={styles.uploudImage}>
-          <img src={Collections} />
-          <h2>Compartilhe seu talento com milhares de pessoas</h2>
-          <input type="file" id="upload" name="upload" />
+      <label htmlFor="uploadImg">
+        <div
+          className={`${styles.uploudImage} ${
+            dataImage ? styles.withImage : ""
+          }`}
+          style={style}
+        >
+          <img src={Collections} style={toggleDisplay} />
+          <h2 style={toggleDisplay}>
+            Compartilhe seu talento com milhares de pessoas
+          </h2>
+          <input
+            type="file"
+            id="uploadImg"
+            name="uploadImg"
+            onChange={handleUpload}
+          />
         </div>
       </label>
     </div>
