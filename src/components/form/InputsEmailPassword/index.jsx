eec42dc373a1249516e754c9d193cc2eab14c1cd
JM-arlopes <jmiguelarlopes@gmail.com>
2024-02-02 09:40:34 -0300

    Atualiza os componentes InputsEmailPassword e Input
    - Adiciona suporte para exibição de senhas ocultas/visíveis no componente InputsEmailPassword
    - Refatora o componente Input para incluir lógica para alternar a visibilidade da senha

--- src/components/form/InputsEmailPassword/index.jsx
+++ src/components/form/InputsEmailPassword/index.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import Input from "../Input";
 
 export default function InputsEmailPassword({ handleOnChange, user }) {
+  const [isPassword, setPassword] = useState(false);
+
+  const togglePassword = () => {
+    setPassword(!isPassword);
+  };
+
   return (
     <>
       <Input
         text="Email adress"
         name="email"
         type="email"
-        placeholder="John@gmail.com"
         handleOnChange={handleOnChange}
         value={user.email ? user.email : ""}
       />
@@ -15,10 +21,12 @@ export default function InputsEmailPassword({ handleOnChange, user }) {
       <Input
         text="Password"
         name="password"
-        type="password"
-        placeholder="3ygyg37ry74hsa"
+        type={!isPassword ? "password" : "text"}
         handleOnChange={handleOnChange}
         value={user.password ? user.password : ""}
+        toggleVisible={togglePassword}
+        isPassword={isPassword}
+        icon={true}
       />
     </>
   );
